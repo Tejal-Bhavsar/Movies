90d92d73ac6472f3b9ba1387d1a7536514a150fb
tejal-bhavsar-au6 <tejalbhavsar02@gmail.com>
2021-01-09 17:11:15 +0530

    completed

--- src/components/Movielist.js
+++ src/components/Movielist.js
@@ -5,7 +5,6 @@ export default function Movielist({movie,getfavmovies,id}) {
     return (
         <>
                <div className="moviecard d-flex justify-content-start m-3"> 
-                  
                     <img src={movie.Poster}></img>
                     <div onClick={() => getfavmovies(movie)} className="overlay d-flex align-items-center justify-content-center">
                         <Favourite /> 
